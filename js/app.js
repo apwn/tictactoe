ff1438a630a4886cd05484ea054e08bdf3421dba
apwn <thomas.kanze@gmail.com>
2016-05-09 10:56:22 +1000

    creation of game area code added

--- js/app.js
+++ js/app.js
@@ -1,21 +1,35 @@
 /**********************************************
 TIC TAC TOE
 Version 0.25
+
+line 16: Creation of Game Area
 **********************************************/
 
 // Wait for page to load before running any JS
 $(function(){
 
-var rows = 3;
-var cols = 3;
+// Number or Rows and colums the game should have
+var numberOfRows = 3;
+var numberOfCols = 3;
+var gameArea = {};
 
-// Creating the game area
-for (var r = 0; r < rows; r++){
-  $('<div>', {class:"row " + "row"+r}).css({width: '60%', height: '50px'}).appendTo('#game');
-  for (var c = 0; c < cols; c++) {
-    $('<div>', {class:"col" + "col"+c}).css({width: '20%', height: '50px', border: '1px solid #000', display: 'inline-block'}).appendTo('.row' + r);
+////////////////////////////////////////////////
+// CREATION OF GAME AREA
+// PUSH ALL ROWS AND COLS TO OBJECT gameArea
+////////////////////////////////////////////////
+
+for (var r = 0; r < numberOfRows; r++){
+  $('<div>', {class:"row " + "row"+r}).css({width: '60%', height: '50px', margin: '0 auto'}).appendTo('#game');
+    gameArea['row'+r] = [];
+  for (var c = 0; c < numberOfCols; c++) {
+    $('<div>', {class:"col" + "col"+c}).css({width: 100/numberOfCols-1 + '%', height: '50px', border: '1px solid #000', display: 'inline-block'}).appendTo('.row' + r);
+    gameArea['row'+r].push('col'+c);
   }
 }
+
+console.log(gameArea);
+
+
 
 
 
