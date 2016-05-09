1e2a8d370fdc9f262791ef24d21bb8bbced070ea
apwn <thomas.kanze@gmail.com>
2016-05-09 12:38:03 +1000

    add player selection to object gamearea

--- js/app.js
+++ js/app.js
@@ -2,7 +2,8 @@
 TIC TAC TOE
 Version 0.25
 
-line 16: Creation of Game Area
+line 21: Creation of Game Area
+line xx: User selection
 **********************************************/
 
 // Wait for page to load before running any JS
@@ -13,6 +14,16 @@ var numberOfRows = 3;
 var numberOfCols = 3;
 var gameArea = {};
 
+// Player info
+var player1 = "Thomas";
+var player2;
+//var playerGame = {};
+
+// Stats
+var playerTotalTurns;
+var player1Turns = [];
+var player2Turns = [];
+
 ////////////////////////////////////////////////
 // CREATION OF GAME AREA
 // PUSH ALL ROWS AND COLS TO OBJECT gameArea
@@ -21,16 +32,36 @@ var gameArea = {};
 for (var r = 0; r < numberOfRows; r++){
   $('<div>', {class:"row " + "row"+r}).css({width: '60%', height: '50px', margin: '0 auto'}).appendTo('#game');
     gameArea['row'+r] = [];
+    //playerGame['row'+r] = [];
   for (var c = 0; c < numberOfCols; c++) {
-    $('<div>', {class:"col" + "col"+c}).css({width: 100/numberOfCols-1 + '%', height: '50px', border: '1px solid #000', display: 'inline-block'}).appendTo('.row' + r);
+    $('<div>', {class:"col " + "col"+c}).css({width: 100/numberOfCols-1 + '%', height: '50px', border: '1px solid #000', display: 'inline-block'}).appendTo('.row' + r);
     gameArea['row'+r].push('col'+c);
   }
 }
 
-console.log(gameArea);
+ console.log(gameArea);
+ //console.log(playerGame);
 
+////////////////////////////////////////////////
+// USER SELECTION
+// SELECT A BOX IN THE GAME
+////////////////////////////////////////////////
 
+function boxSelected(){
+  var col = this.className;
+  var row = this.parentElement.className;
+  var col2 = col.split(' ');
+  var row2 = row.split(' ');
+  var playerCol = col2[1];
+  var playerRow = row2[1];
+  var elementToChange = gameArea[playerRow].indexOf(playerCol)
+  gameArea[playerRow][elementToChange] = player1;
+  // add function to check winner
+}
 
+$('.col').on('click', boxSelected);
+
+//console.log(classes2);
 
 
 
